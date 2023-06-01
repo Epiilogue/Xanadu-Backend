c5da27c9009a2dc9e3291830f40775764a739ad7
GaosongXu <shimaring@qq.com>
2023-06-02 00:35:29 +0800

    设置数据源，启动模块

---
+++ ruoyi-modules/xanadu-customer-center/src/main/java/edu/neu/cc/CustomerCenterApplication.java
@@ -0,0 +1,12 @@
+package edu.neu.cc;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCenterApplication {
+    public static void main(String[] args) {
+        //启动Spring
+        org.springframework.boot.SpringApplication.run(CustomerCenterApplication.class, args);
+    }
+}
