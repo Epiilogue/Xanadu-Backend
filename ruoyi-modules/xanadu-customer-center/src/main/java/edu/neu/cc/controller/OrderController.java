9153ef930d373a099ddba80c191a0a2e9636c33f
GaosongXu <shimaring@qq.com>
2023-06-04 16:11:22 +0800

    添加统一跨域配置

--- ruoyi-modules/xanadu-customer-center/src/main/java/edu/neu/cc/controller/OrderController.java
+++ ruoyi-modules/xanadu-customer-center/src/main/java/edu/neu/cc/controller/OrderController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
- *  前端控制器
- * </p>
  *
  * @author Gaosong Xu
  * @since 2023-06-01 11:10:58
@@ -16,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cc/order")
 public class OrderController {
+
+
+
+
 
 }
 
