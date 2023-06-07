3512dd7d68c1cedf6bada1e2d5dbdeffcbf4bd4b
GaosongXu <shimaring@qq.com>
2023-06-07 09:26:26 +0800

    更新gitignore文件，添加对配置文件的排除规则

--- ruoyi-modules/xanadu-distribution-center/src/main/java/edu/neu/dbc/controller/PurchaseRecordController.java
+++ ruoyi-modules/xanadu-distribution-center/src/main/java/edu/neu/dbc/controller/PurchaseRecordController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/dbc/purchaseRecord")
 public class PurchaseRecordController {
+    //生成采购单，需要传入的为缺货单，生成采购单后，将所有涉及到的采购单状态置为已采购
+    //可以修改采购数量，但是采购数量不可以小于缺货数量也不可以大于最大库存量
+
 
 }
 
