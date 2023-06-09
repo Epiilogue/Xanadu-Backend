9bf9b347a2bf29e3f5791c0a28f2d6b6e803db06
GaosongXu <shimaring@qq.com>
2023-06-09 14:23:12 +0800

    修复仓库模块的跨域、分页bean装配问题

--- ruoyi-modules/xanadu-warehouse/src/main/java/edu/neu/ware/WarehouseApplication.java
+++ ruoyi-modules/xanadu-warehouse/src/main/java/edu/neu/ware/WarehouseApplication.java
@@ -3,7 +3,7 @@ package edu.neu.ware;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"edu.neu"})
 @EnableFeignClients
 public class WarehouseApplication {
         public static void main(String[] args) {
