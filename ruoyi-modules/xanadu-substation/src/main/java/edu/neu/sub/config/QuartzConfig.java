9ed7b256fba5086121f3602034301635efbc7aa1
GaosongXu <shimaring@qq.com>
2023-07-01 19:18:24 +0800

    每日自动生成报表，实现报表生成接口的幂等性

---
+++ ruoyi-modules/xanadu-substation/src/main/java/edu/neu/sub/config/QuartzConfig.java
@@ -0,0 +1,28 @@
+package edu.neu.sub.config;
+
+import edu.neu.sub.Job.DailyReportJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetail jobDetail() {
+        return JobBuilder.newJob(DailyReportJob.class)
+                .withIdentity("daily_report", "daily_report")
+                .build();
+    }
+
+    @Bean
+    public Trigger trigger() {
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail())
+                .withIdentity("daily_report", "daily_report")
+                .startNow()
+                // 每天八点自动生成当日的结算报表
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 0 20 * * ? *"))
+                .build();
+    }
+}
